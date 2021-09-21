0a7447fd6bd6a81721f8ba86723778e9e83a98c6
Walla-B <81977508+Walla-B@users.noreply.github.com>
2021-09-21 16:06:23 +0900

    OpenGL project added

---
+++ OpenGL_Test1/OpenGL_Test1/OpenGL_Test1.h
@@ -0,0 +1,40 @@
+﻿
+// OpenGL_Test1.h: OpenGL_Test1 애플리케이션의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// COpenGLTest1App:
+// 이 클래스의 구현에 대해서는 OpenGL_Test1.cpp을(를) 참조하세요.
+//
+
+class COpenGLTest1App : public CWinAppEx
+{
+public:
+	COpenGLTest1App() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 구현입니다.
+	UINT  m_nAppLook;
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern COpenGLTest1App theApp;
